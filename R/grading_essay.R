05422b6d74bd2ea1f0c4fe24261a47b51ddbca4e
NMarkgraf <nmarkgraf@hotmail.com>
2023-08-29 10:08:42 +0200

    A little bit of happyness ...

--- R/grading_essay.R
+++ R/grading_essay.R
@@ -1,7 +1,3 @@
-
-
-
-
 #' grade essay HS Ansbach
 #'
 #' takes a dataframe with 4 columns and grades it
@@ -22,36 +18,32 @@
 #' @examples `grade_essay(d)`
 #'
 grade_essay_hsan <- function(d) {
-
-
-
-  # parse numbers:
-  out <-
-    d |>
-    # parse numbers:
-    dplyr::mutate(dplyr::across(.cols = c(Formales, Methodik, Inhalt),
-                                .fns = ~ readr::parse_number(
-                                  ., locale = readr::locale(decimal_mark = ",")))) |>
-    # compute rowwise average:
+#  out <- ## Implzit return used!
+    d |> # parse numbers:
+    dplyr::mutate(
+      dplyr::across(
+        .cols = c(Formales, Methodik, Inhalt),
+        .fns = ~ readr::parse_number(
+            ., locale = readr::locale(decimal_mark = ",")
+        )
+      )
+    ) |> # compute rowwise average:
     dplyr::rowwise() |>
-    dplyr::mutate(grade_avg = base::mean(c(Formales, Methodik, Inhalt))) |>
-    dplyr::ungroup() |>
-
-    # add bonus:
-    dplyr::mutate(grade_avg2 =
-                    dplyr::case_when(
-                      Bonus == "ja" ~ grade_avg - 0.33,
-                      TRUE ~ grade_avg)) |>
-
-    # correct for "fail anyway"
-    dplyr::mutate(grade_avg3 = ifelse(Formales > 4 | Methodik > 4 | Inhalt > 4, 5, grade_avg2)) |>
-
-    # round according to rounding scheme:
-    dplyr::mutate(grade_avg4 = round_grades(grade_avg3)) |>
-
-    # return numeric vector:
+    dplyr::mutate(
+      grade_avg = base::mean(c(Formales, Methodik, Inhalt))
+    ) |>
+    dplyr::ungroup() |> # add bonus:
+    dplyr::mutate(
+      grade_avg2 = dplyr::case_when(
+        Bonus == "ja" ~ grade_avg - 0.33,
+        TRUE ~ grade_avg)) |> # correct for "fail anyway"
+    dplyr::mutate(
+      grade_avg3 = ifelse(Formales > 4 | Methodik > 4 | Inhalt > 4,
+                          5,
+                          grade_avg2)
+    ) |> # round according to rounding scheme:
+    dplyr::mutate(
+      grade_avg4 = round_grades(grade_avg3)
+    ) |> # return numeric vector:
     dplyr::pull(grade_avg4)
-
-
-
 }
